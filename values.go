99f374332f622cf5099d83fbe520d870c7a2a424
Arthur Zettler <arthur.zettler@corp.globo.com>
2017-03-15 18:43:01 -0300

    feat: values example

---
+++ values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("go" + "lang") // golang
+
+    fmt.Println("1 + 1 =", 1 + 1) // 1 + 1 = 2
+    fmt.Println("2.0/3.0 =", 2.0/3.0) // 2.0/3.0 = 0.6666666666666666
+
+    fmt.Println(true && false) // false
+    fmt.Println(true || false) // true
+    fmt.Println(!true) // false
+}
